bb46f7b3713958b81451da60b8ec8eb40c03cfd5
Kamil Janowski <kamil.janowski@reaktor.com>
2024-06-17 09:45:12 +0300

    updated the docs

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,12 @@ import { test, describe, expect } from "vitest";
 import { toIPv4 } from "./index";
 
 describe("toIPv4", () => {
+  test("for -1 should throw an error", () => {
+    expect(() => toIPv4(-1)).toThrow(
+      "Invalid IP address. Must be between 0 and 9007199254740991.",
+    );
+  });
+
   test("should convert 0 to 0.0.0.0", () => {
     expect(toIPv4(0)).toBe("0.0.0.0");
   });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+/**
+ * An IP v4 address is a 32-bit number that is usually represented as a string in the form of `a.b.c.d` where `a`, `b`, `c`, and `d` are integers between 0 and 255.
+ *
+ * The following function converts a 32-bit number to an IP v4 address.
+ * @param ip - The IP address in a format of a 32-bit number - must be between 0 and 9007199254740991 (Number.MAX_SAFE_INTEGER)
+ */
 export const toIPv4 = (ip: number) => {
+  if (ip < 0 || ip > Number.MAX_SAFE_INTEGER) {
+    throw new Error(
+      "Invalid IP address. Must be between 0 and 9007199254740991.",
+    );
+  }
   return (
     ((ip >> 24) & 0xff) +
     "." +
@@ -8,4 +19,4 @@ export const toIPv4 = (ip: number) => {
     "." +
     (ip & 0xff)
   );
-}
+};
